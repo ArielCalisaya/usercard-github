bcbee392d73ac38faf635eee0f9ee7d541d4856a
ArielC <calisayaariel62@gmail.com>
2020-09-13 00:09:52 -0300

    almost finished

--- src/App.js
+++ src/App.js
@@ -4,36 +4,33 @@ import Card from "./components/card";
 import { useForm } from "react-hook-form";
 
 function App() {
-
   // React-Hook-Form handler
   const { register, handleSubmit, errors } = useForm();
-  const [ username, setUsername ]= useState("");
+  const [username, setUsername] = useState("");
 
   const onSubmit = (data, e) => {
     console.log(data);
-    e.target.reset()
+    e.target.reset();
 
-    setUsername(data.searchUser)
+    setUsername(data.searchUser);
   };
 
   return (
     <section className="gradient-bg min-h-screen text-white flex justify-center items-center flex-col">
       <div>
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <form onSubmit={handleSubmit(onSubmit)} className="text-center">
+          {errors.searchUser && errors.searchUser.type === "required" && (
+            <p>It's Required some text to find User!</p>
+          )}
           <input
-            className="text-black"
+            className="text-black input-key-search text-white"
             name="searchUser"
             type="text"
             placeholder="Search Github User"
-            ref={register({ required: true})}
+            ref={register({ required: true })}
           />
-          {errors.searchUser && errors.searchUser.type === "required" && (
-            <p>This is Required</p>
-          )}
         </form>
-        {
-          username ? <Card userName={username}/> : null
-        }
+        {username === "" ? null : <Card userName={username} />}
       </div>
     </section>
   );
--- src/components/card.js
+++ src/components/card.js
@@ -2,47 +2,77 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const Card = (props) => {
-
-    const {userName} = props
+  const { userName } = props;
 
   const [user, setUser] = useState([]);
+  const [repository, setRepo] = useState([]);
 
   useEffect(() => {
-    const apiURL = `https://api.github.com/users/${userName}`;
-    axios.get(apiURL).then((res) => {
-      console.log(res.data);
-      setUser(res.data);
-    });
+    const userAPI = `https://api.github.com/users/${userName}`;
+    const reposAPI = `https://api.github.com/users/${userName}/repos`;
+    axios
+      .get(userAPI)
+      .then((res) => {
+        setUser(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    axios
+      .get(reposAPI)
+      .then((res) => {
+        console.log(res.data);
+        setRepo(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
     return () => {
       //
     };
   }, [userName]);
 
-  //   const getUser = (user) => {
-  //     const apiURL = "https://api.github.com/users/"+ user;
-  //     axios.get(apiURL).then((res) => {
-  //     //   setUser(res.data);
-  //     });
-  //   }
-
-//   WOOOOOOOORKSSS YESSSSSSSSSSSS!!!
   return (
-    <div>
-        <p>userNameKey: {userName}</p>
-
-
-
-      <p>{user.name}</p>
-      <div>
-        <img src={user.avatar_url} alt="user-avatar" />
+    <div className="card">
+      <div className="img-container">
+        <img
+          className="avatar h-auto w-auto"
+          src={user.avatar_url}
+          alt={user.name}
+        />
       </div>
-      <div>
+      <div className="user-info">
         <h2>{user.name}</h2>
         <p>{user.bio}</p>
-        <ul>
-          <li>{user.followers}</li>
-          <li>{user.following}</li>
-          <li>{user.public_repos}</li>
+        <ul className="info">
+          <li>
+            {user.followers}
+            <strong>followers</strong>
+          </li>
+          <li>
+            {user.following}
+            <strong>following</strong>
+          </li>
+          <li>
+            {user.public_repos}
+            <strong>Repositories</strong>
+          </li>
+        </ul>
+        <ul className="repos contents">
+          {repository.map((repo, key) => {
+            return (
+              <a
+                key={key}
+                href={repo.html_url}
+                rel="noopener noreferrer"
+                target="_blank"
+                className="repo"
+              >
+                {repo.name}
+              </a>
+            );
+          })}
         </ul>
       </div>
     </div>
